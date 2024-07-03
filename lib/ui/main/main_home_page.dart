d33ce3b3b8fe984ee0abf4528252116517a224dd
loipham <loipham21101998@gmail.com>
2024-07-03 14:08:59 +0700

    multiple languages

--- lib/ui/main/main_home_page.dart
+++ lib/ui/main/main_home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quanlychitieu/ui/main/home_page_child_NKCT.dart';
 import 'package:quanlychitieu/ui/main/home_page_child_TC.dart';
-
+import 'package:easy_localization/easy_localization.dart';
 import '../../ultils/enums/main_page.dart';
 import 'home_page_top.dart';
 
@@ -23,18 +23,18 @@ class _MainHomePageState extends State<MainHomePage> {
     // TODO: implement initState
     super.initState();
     _pages = [
-      const HomePage(key : ValueKey(1),body:HomePageChildTc()),
-      const HomePage(key: ValueKey(2),body: HomePageChildNkct()),
+      const HomePage(key: ValueKey(1), body: HomePageChildTc()),
+      const HomePage(key: ValueKey(2), body: HomePageChildNkct()),
       Container(
-       key:  const ValueKey(3),
+        key: const ValueKey(3),
         color: Colors.yellow,
       ),
       Container(
-        key:   const ValueKey(4),
+        key: const ValueKey(4),
         color: Colors.blue,
       ),
       Container(
-        key:  const ValueKey(5),
+        key: const ValueKey(5),
         color: Colors.purple,
       )
     ];
@@ -47,15 +47,18 @@ class _MainHomePageState extends State<MainHomePage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(_mainPage.TitleAppBar()),
+        actions: [DropdownLang(contextMain: context,)],
         backgroundColor: const Color(0xff14D210),
       ),
       body: AnimatedSwitcher(
           duration: const Duration(microseconds: 1000),
           transitionBuilder: (Widget child, Animation<double> animation) {
-           return FadeTransition(opacity: animation,child: child,);
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
           },
-          child: _pages.elementAt(_currentPage)
-      ),
+          child: _pages.elementAt(_currentPage)),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xffFFFFFF),
         unselectedItemColor: const Color(0xffC0C0C0),
@@ -103,6 +106,67 @@ class _MainHomePageState extends State<MainHomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+final List<String> langs = ['vi', 'en'];
+
+class DropdownLang extends StatefulWidget {
+  BuildContext contextMain;
+  DropdownLang({super.key ,required this.contextMain});
+
+  @override
+  State<DropdownLang> createState() => _DropdownLangState();
+}
+
+class _DropdownLangState extends State<DropdownLang> {
+
+  late String dropdownValue ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    dropdownValue = langs.where((v) => v == widget.contextMain.locale.toString()).first;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 14.5),
+      child: DropdownButton<String>(
+        value: dropdownValue,
+        iconSize: 0,
+        icon:
+            const Visibility(visible: false, child: Icon(Icons.arrow_downward)),
+        onChanged: (String? value) {
+          widget.contextMain.setLocale(Locale(value!));
+          setState(() {
+            dropdownValue = value;
+          });
+        },
+        underline: Container(
+          height: 2,
+          color: Colors.transparent,
+        ),
+        items: langs.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            alignment: Alignment.center,
+            value: value,
+            child: _iconLang(value),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _iconLang(String lang) {
+    return Container(
+      width: 30,
+      height: 20,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: lang == 'vi' ? Colors.red : Colors.blue),
     );
   }
 }
